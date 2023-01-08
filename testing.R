6b61c4623430260c85e287ca55d7099d92eb15e4
Moshey96 <elmoushey@gmail.com>
2023-01-08 16:40:02 +0200

    why this is not showing in GitHub WTF

--- testing.R
+++ testing.R
@@ -4,3 +4,6 @@ Print("and now it lives on GitHyub")
 
 print("now this me trying new stuff to check if everything is working")
 
+print("there si something wrong")
+ 
+print("its not showing in GitHub")
